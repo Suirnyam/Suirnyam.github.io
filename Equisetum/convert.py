db69c93c6fc18fe2630fc3c294c9bdfb7f0fc183
Berusska <karasek.s54@gmail.com>
2023-06-29 16:00:30 +0200

    Deletion

--- Equisetum/convert.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-
-sourcedir = Path(".")
-outputdir = Path("./output")
-
-for file in sourcedir.glob("*.jpg"): print(f"ffmpeg -i {file.name} -vf scale=20:-1 {file.stem}_.jpg")
-
-
